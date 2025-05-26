7d07ffe8e0f9707fc35642f3bd68ab9a6cbcabe5
Yogeshwaran B <yogiwar33@gmail.com>
2025-05-26 17:57:34 +0530

    three

---
+++ client/src/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+const navbar = () => {
+  return (
+    <div className="">
+        <div>DAYNOTES</div>
+        <button className="bg-transparent backdrop-blur-[20px] shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] rounded-[10px] border border-white/20">Login</button>
+    </div>
+  )
+}
+export default navbar
