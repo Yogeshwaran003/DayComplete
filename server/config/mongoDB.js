2d280faada032186970c449ab7cfb693767b6377
Yogeshwaran B <yogiwar33@gmail.com>
2025-05-27 11:28:48 +0530

    Updated the Mongo DB

---
+++ server/config/mongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  
+    mongoose.connection.on("connected", () => {
+        console.log("connected to database successfully");
+    });
+
+    await mongoose.connect(`${process.env.MONGODB_URL}/TaskManageBase`);
+  
+};
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import 'dotenv/config'
+import connectDB from './config/mongoDB.js'
+const app = express()
+app.use(express.json())
+
+
+connectDB()
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello world</h1>")
+})
+
+app.listen(4000,()=>{
+    console.log("server is connected")
+})
